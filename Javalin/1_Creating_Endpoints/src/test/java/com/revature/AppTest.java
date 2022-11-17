cf0c08cf4b90ee1f8b2c84c87d51088fb4b01db2
Kevin Childs <kchilds2020@gmail.com>
2022-11-17 12:42:54 -0600

    added Javalin Singleton for tests

--- Javalin/1_Creating_Endpoints/src/test/java/com/revature/AppTest.java
+++ Javalin/1_Creating_Endpoints/src/test/java/com/revature/AppTest.java
@@ -6,25 +6,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.revature.util.CommandLine;
+
+import io.javalin.Javalin;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    Javalin app = JavalinSingleton.getSingleton();
+    @Before
+    public void beforeEach() throws InterruptedException{
+        app.start(9001);
+        Thread.sleep(3000);
+    }
+
+    @After
+    public void afterEach(){
+        app.stop();
+    }
+
     @Test
     public void shouldAnswerWithTrue()
     {
         String expectedResult = "Hello World";
 
-        String actualResult = CommandLine.executeCommandPrompt("curl http://localhost:9000/hello");
+        String actualResult = CommandLine.executeCommandPrompt("curl http://localhost:9001/hello");
 
 
         if(actualResult.isEmpty()){
