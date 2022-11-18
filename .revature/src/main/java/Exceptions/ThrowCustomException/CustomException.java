3f980b769d5ff477089f1635c3079c66a214b797
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2022-11-18 12:34:46 -0500

    Okay

--- .revature/src/main/java/Exceptions/ThrowCustomException/CustomException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions.ThrowCustomException;
-
-/**
- * This class represents a custom exception which may be thrown in the CustomExceptionThrower class.
- */
-public class CustomException extends Exception{
-}
