80af14c2adaf3e274ce97bdda421266b9f2e8c6a
tedbeast <theodore.balashov@revature.com>
2022-12-13 09:44:41 -0500

    re-added combinestrings lab

---
+++ CombineStrings/src/main/Java/CombineStrings.java
@@ -0,0 +1,13 @@
+public class CombineStrings {
+    /**
+     * You can combine two strings using the '+' operator.
+     * For instance, String bigstr = str1 + str2.
+     * This method should return the combination of two Strings.
+     * @param a the first part of the string to be combined.
+     * @param b the second part of the string to be combined.
+     * @return the combination of strings a and b.
+     */
+    public String combine(String a, String b){
+        return "";
+    }
+}
