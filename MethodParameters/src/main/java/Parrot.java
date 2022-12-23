a887c9a9519df0d0028c7ef32853da934b05fa00
melscottjr77 <melscottjr77@gmail.com>
2022-12-23 06:47:28 +0000

    Saved progress on MethodParameters 2022-12-23 06:47:28 +0000

--- MethodParameters/src/main/java/Parrot.java
+++ MethodParameters/src/main/java/Parrot.java
@@ -1,7 +1,8 @@
 
 public class Parrot {
     /**
-     * This method should return whatever String was passed into it by whatever invoked it (in this case, the test),
+     * This method should return whatever String was passed into it by whatever
+     * invoked it (in this case, the test),
      * instead of returning "return text please!"
      *
      * for example:
@@ -11,8 +12,11 @@ public class Parrot {
      * @param text is a parameter passed into this method by whatever called it.
      * @return text.
      */
-    public static void main(String[] args){}
-    public String copy(String text){
-        return("text"); 
+    public static void main(String[] args){
+        String hello = ("hello");
+        System.out.println(hello);
+    }
+    public String copy(String textrevpro ){
+        return("hello");
     }
 }
