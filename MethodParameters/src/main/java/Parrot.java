2c719b4749ca35b403df52ccfdf8d32dbee54a20
melscottjr77 <melscottjr77@gmail.com>
2023-01-01 19:01:53 +0000

    Saved progress on MethodParameters 2023-01-01 19:01:53 +0000

--- MethodParameters/src/main/java/Parrot.java
+++ MethodParameters/src/main/java/Parrot.java
@@ -15,7 +15,7 @@ public class Parrot {
      */
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String str = ("hello");
+        String str = sc.nextLine();
         System.out.println(str);
     }
 }
