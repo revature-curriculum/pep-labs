45d7fc6823b7a6791ec50216ca486aac0b448c4a
Kaivon K Tolooee <kaivont@gmail.com>
2023-01-09 02:33:09 +0000

    Saved progress on ForLoop 2023-01-09 02:33:09 +0000

--- ForLoop/src/main/java/ForLoop.java
+++ ForLoop/src/main/java/ForLoop.java
@@ -42,8 +42,9 @@ public class ForLoop {
     public String returnNumbers(int start, int end){
         String buildingString = "";
 
-//        code here
+        for(int i = start; i < end; i++) { buildingString += i + " ";
+    }return buildingString;
 
-        return buildingString;
+       
     }
 }
